269b968da9a6a1d9992b56c7d5ab91fdd3d82328
rafaeldasilvacabral <rscd.canada@gmail.com>
2025-03-31 22:15:40 -0300

    Update mail_handler.php

--- mail_handler.php
+++ mail_handler.php
@@ -1,21 +1,20 @@
-<?php 
-if(isset($_POST['submit'])){
-    $to = "rscabral.canada@gmail.com"; // this is your Email address
-    $from = $_POST['email']; // this is the sender's Email address
-    $first_name = $_POST['first_name'];
-    //$last_name = $_POST['last_name'];
-    $subject = "Form submission";
-    //$subject2 = "Copy of your form submission";
-    $msg = $_POST['message'];
-    $message = $first_name . " " . " wrote the following:" . "\n\n" . $msg;
-    //$message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
+<!-- envio_email.php -->
+<?php
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $mensagem = $_POST['mensagem'];
+    $sub = $_POST['subject'];
 
-    $headers = "From:" . $from;
-    $headers2 = "From:" . $to;
-    mail($to,$subject,$message,$headers);
-    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
-    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
-    // You can also use header('Location: thank_you.php'); to redirect to another page.
-    // You cannot use header and echo together. It's one or the other.
+    $to = 'rscabral.canada@gmail.com';
+    $subject = $sub . ' ' . $nome;
+    $body = "Nome: $nome\nEmail: $email\n\nMensagem:\n$mensagem";
+    $headers = "From: $email";
+
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Email enviado com sucesso!";
+    } else {
+        echo "Falha no envio do email.";
     }
+}
 ?>
